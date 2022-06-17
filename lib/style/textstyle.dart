a69aa5eb58a1d67d3b7fd9ea22054f77a0ff143b
Adetoba <adetoba53@gmail.com>
2022-06-17 15:27:08 +0100

    create text styles for general use

---
+++ lib/style/textstyle.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:test_delivery_app/utils/dimensions.dart';
+
+
+class CustomStyles {
+
+  static SizeConfig config = SizeConfig();
+
+  static TextStyle bold24 = TextStyle(
+    fontSize: config.sp(24),
+    fontWeight: FontWeight.w600,
+    color: const Color(0xff041C2E)
+  );
+
+  static TextStyle bold18 = TextStyle(
+    fontSize: config.sp(18),
+    fontWeight: FontWeight.w700,
+    color: const Color(0xff092C4C)
+  );
+
+  static TextStyle normal14 = TextStyle(
+    fontSize: config.sp(14),
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+    color: const Color(0xff96823D)
+  );
+}
