2904c29fc66a84b9be61c921547a237f24419cdb
Adetoba <adetoba53@gmail.com>
2022-06-17 14:41:46 +0100

    init google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:test_delivery_app/pages/login.dart';
 
 void main() {
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: GoogleFonts.inter().fontFamily,
         primarySwatch: Colors.blue,
       ),
       home: const LoginPage()
