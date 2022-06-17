c8a38c42e58dbb350e8ca3ce9b744b9baa1a85d9
Adetoba <adetoba53@gmail.com>
2022-06-17 14:38:24 +0100

    chore: import all required assets and add dependencies

---
+++ lib/style/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Palette {
+  static const Color white = Colors.white;
+  static const Color loginBg = Color(0xffFFD337);
+
+  static const Color deepLoginBg = Color(0xffFFCE23);
+  static const Color black = Color(0xff000000);
+
+}
